c5587024fbdc3fd26d48a8e5d6e67476f9b8415c
shamim-hasan259 <mdshamimhasan791@gmail.com>
2026-06-20 20:15:57 +0600

    verify token middlewere complete

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const dontenv = require("dotenv");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 dontenv.config();
 
 const uri = process.env.MONGO_URI;
@@ -12,12 +13,12 @@ const uri = process.env.MONGO_URI;
 const app = express();
 const PORT = process.env.PORT;
 
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: [process.env.CLIENT_URI],
-//   }),
-// );
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.CLIENT_URI],
+  }),
+);
 
 app.use(cors());
 app.use(express.json());
@@ -29,24 +30,88 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+const JWKS = createRemoteJWKSet(
+  new URL(`${process.env.CLIENT_URI}/api/auth/jwks`),
+);
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
 
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res.status(401).json({ msg: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  // console.log(token);
+
+  if (!token) {
+    return res.status(401).json({ msg: "Unauthorized" });
+  }
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    req.user = payload;
+    next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ msg: "Unauthorized" });
+  }
+};
+
+const verifyUser = (req, res, next) => {
+  if (req.user.role !== "user") {
+    return res.status(403).json({ status: false, message: "Forbidden" });
+  }
+  next();
+};
+
+const verifyAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ status: false, message: "Forbidden" });
+  }
+  next();
+};
 async function run() {
   try {
     await client.connect();
     const recipeHubDB = client.db("recipe-hub");
     const recipesCollection = recipeHubDB.collection("recipes");
+    const userCollection = recipeHubDB.collection("user");
 
-    app.post("/api/recipes", async (req, res) => {
-      const result = await recipesCollection.insertOne(req.body);
-      res
-        .status(201)
-        .json({
-          status: true,
-          message: "recipe created successfully",
-          data: result,
-        });
+    // user related api
+
+    app.patch("/api/user/:id", async (req, res) => {
+      const { id } = req.params;
+
+      const result = await usersCollection.updateOne(
+        { _id: new ObjectId(id) },
+        {
+          $inc: {
+            limit: 1,
+          },
+        },
+      );
+
+      res.status(200).json({
+        success: true,
+        message: "Limit increased successfully",
+        data: result,
+      });
     });
 
+    // public api
+    app.get("/api/allrecipes", async (req, res) => {});
+
+    app.post("/api/recipes", verifyToken, verifyUser, async (req, res) => {
+      const body = req.body;
+      const data = {
+        ...body,
+        createdAt: new Date(),
+      };
+      const result = await recipesCollection.insertOne(data);
+      res.status(201).json({
+        status: true,
+        message: "recipe created successfully",
+        data: result,
+      });
+    });
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
